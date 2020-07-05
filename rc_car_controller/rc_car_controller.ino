36cfe5eeedf16deb32ff079bb672d7a0683e97cb
Joey Herguth <oitzujoey@gmail.com>
2020-07-05 00:35:15 -0700

    Modify to work with steering only on an Arduino Mega

--- rc_car_controller/rc_car_controller.ino
+++ rc_car_controller/rc_car_controller.ino
@@ -1,8 +1,7 @@
 
 #include <stdio.h>
 #include <SoftwareSerial.h>
-
-#define ANALOG_STEERING
+#include <Servo.h>
 
 SoftwareSerial HC05UART(8, 9);  //RX, TX
 
@@ -15,10 +14,7 @@ char get_char();
 int16_t hex_to_int(char *);
 void move_motors(direction_t);
 
-const int forward = 3;
-const int back = 5;
-const int left = 6;
-const int right = 11;
+const int steering = 13;
 
 const char axes = 2;
 char data, high, low;
@@ -26,18 +22,19 @@ const char buf_size = 2*sizeof(int16_t) + 1;
 char buf[buf_size];
 direction_t direction;
 
+Servo servo;
+
 void setup() {
   
-  Serial.begin(115200);
+  Serial.begin(9600);
+  Serial1.begin(9600);
 
   buf[buf_size] = '\0';
 
-  pinMode(forward, OUTPUT);
-  pinMode(back, OUTPUT);
-  pinMode(right, OUTPUT);
-  pinMode(left, OUTPUT);
+  servo.attach(steering, 1300, 1700);
+  servo.writeMicroseconds(1500);
   
-  HC05UART.begin(9600);
+//  HC05UART.begin(9600);
   
   while (!Serial);
 }
@@ -47,7 +44,7 @@ void loop() {
   int j;
   
   if (Serial.available() > 0) {
-    HC05UART.write(Serial.read());
+    Serial1.write(Serial.read());
   }
 
   while (get_char() != '\n');
@@ -73,58 +70,12 @@ void loop() {
 void move_motors(direction_t direction) {
 
   //  Take the absolute value and constrain it to 0-255.
-  int norm_abs_x = direction.x < 0 ? -direction.x : direction.x;
-  int norm_abs_y = direction.y < 0 ? -direction.y : direction.y;
-  norm_abs_x >>= 7;
-  norm_abs_y >>= 7;
+  int norm_abs_x = direction.x;
+  norm_abs_x >>= 5;
+  norm_abs_x += 1500;
 
-#ifdef ANALOG_STEERING
-  if (direction.x > 0) {
-#else
-  if (direction.x > (2<<13)) {
-#endif
-#ifdef ANALOG_STEERING
-    analogWrite(right, (uint8_t) norm_abs_x);
-#else
-    digitalWrite(right, HIGH);
-#endif
-  } else {
-#ifdef ANALOG_STEERING
-    analogWrite(right, 0);
-#else
-    digitalWrite(right, LOW);
-#endif
-  }
-
-#ifdef ANALOG_STEERING
-  if (direction.x < 0) {
-#else
-  if (direction.x < -(2<<13)) {
-#endif
-#ifdef ANALOG_STEERING
-    analogWrite(left, (uint8_t) norm_abs_x);
-#else
-    digitalWrite(left, HIGH);
-#endif
-  } else {
-#ifdef ANALOG_STEERING`
-    analogWrite(left, 0);
-#else
-    digitalWrite(left, LOW);
-#endif
-  }
-  
-  if (direction.y > 0) {
-    analogWrite(forward, (uint8_t) norm_abs_y);
-  } else {
-    analogWrite(forward, 0);
-  }
-  
-  if (direction.y < 0) {
-    analogWrite(back, (uint8_t) norm_abs_y);
-  } else {
-    analogWrite(back, 0);
-  }
+  Serial.println(norm_abs_x);
+  servo.writeMicroseconds(norm_abs_x);
 }
 
 uint16_t hex_lookup(char hex_char) {
@@ -135,6 +86,7 @@ uint16_t hex_lookup(char hex_char) {
   } else if (('A' <= hex_char) && (hex_char <= 'F')) {
     return (int16_t) hex_char - 'A' + 10;
   }
+  return 0;
 }
 
 int16_t hex_to_int(char *hex) {
@@ -156,9 +108,12 @@ char get_char() {
   char data;
   
   do {
-    while (HC05UART.available() == 0);
+    while (Serial1.available() == 0)
+      Serial.write(' ');
+//    Serial.write('2');
     
-    data = HC05UART.read();
+    data = Serial1.read();
+//    Serial.write('3');
   } while ((data == 0xff) || (data == EOF));  
 
   return data;
